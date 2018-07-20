034a7727c59982156e55ee5b87f7e06f6f78ee10
liangsihuang <380036805@qq.com>
2018-07-20 22:17:04 +0800

    Domain::addElement

--- domain/Domain.py
+++ domain/Domain.py
@@ -2,26 +2,48 @@ from tagged.MapOfTaggedObject import MapOfTaggedObject
 
 class Domain(object):
     def __init__(self):
-        self.theElements = MapOfTaggedObject()
-        self.theNodes    = MapOfTaggedObject()
-        self.theSPs      = MapOfTaggedObject()
-        self.thePCs      = MapOfTaggedObject()
-        self.theMPs      = MapOfTaggedObject()
-        self.theLoadPatterns = MapOfTaggedObject()
-        self.theParameters   = MapOfTaggedObject()
+        self._theElements = MapOfTaggedObject()
+        self._theNodes    = MapOfTaggedObject()
+        self._theSPs      = MapOfTaggedObject()
+        self._thePCs      = MapOfTaggedObject()
+        self._theMPs      = MapOfTaggedObject()
+        self._theLoadPatterns = MapOfTaggedObject()
+        self._theParameters   = MapOfTaggedObject()
 
 # methods to populate a domain
     def addNode(self, node):
         nodTag = node.getTag()
         # 先要看看 theNodes 里面有没有已经存在 node，输入 node 的节点号 nodTag
-        if(theNodes.hasComponent(nodTag)):
-            print('node with tag'+str(nodTag)+'already exist in domain./n' )
-            else:
-                theNodes.addCompenent(node)
+        if(self._theNodes.hasComponent(nodTag)):
+            print('node with tag '+str(nodTag)+' already exist in domain./n' )
+        else:
+            self._theNodes.addComponent(node)
+            node.setDomain(self)
+
+        # 还要： see if the physical bounds are changed
     
     def addElement(self, element):
         eleTag = element.getTag()
         # check all the element nodes exists in the domain
+        nodes = element.getExternalNodes()
+        for i in ragne(0,nodes.len()):
+            if(self._theNodes.hasComponent(nodes[i]):
+                pass
+            else:
+                print('WARNING Domain::addElement - In element '+str(eleTag))
+                print('\n no Node '+str(nodes[i])+' exists in the domain.\n')
+        # check if an Element with a similar tag already exists in the Domain
+        if(self._theElements.hasComponent(eleTag)):
+            print('element with tag '+str(eleTag)+' already exist in domain./n')
+        else:
+            self._theElements.addComponent(element)
+            element.setDomain(self)
+        
+        # 还要：
+        # finally check the ele has correct number of dof
+        # mark the Domain as having been changed
+
+
 
         
     
--- domain/component/DomainComponent.py
+++ domain/component/DomainComponent.py
@@ -2,10 +2,16 @@ from tagged.TaggedObject import TaggedObject
 from actor.MovableObject import MovableObject
 
 class DomainComponent(TaggedObject, MovableObject):
+    
     def __init__(self, tag, clasTag):
         # _theTag 继承自 TaggedObject
         # _classTag 继承自 MovableObject
         self._theTag = tag
         self._classTag = clasTag
+        self._theDomain = None
+    
+    def setDomain(self, model):
+        self._theDomain = model
+
 
 
