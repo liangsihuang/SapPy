9240c810cff6f5636af03dfe0e71cb8366be9db7
liangsihuang <380036805@qq.com>
2018-09-20 22:51:42 +0800

    None is only one

--- TESTING/test_is.py
+++ TESTING/test_is.py
@@ -1,5 +1,10 @@
 from SRC.domain.domain.Domain import Domain
 a = Domain()
 
-if a is Domain:
-    print('1')
+if isinstance(a, Domain):
+    print('1')  # 打印成功
+
+# isinstance() 考虑继承：子类也是父类的类型
+
+print(a is Domain)  # 打印 False
+
